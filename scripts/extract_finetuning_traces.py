d5a68c59494f0e334cecde1e4f29007a72b7c474
Gang Li <ligang@orby.ai>
2025-04-01 16:12:51 -0700

    Update gold trace script

--- scripts/extract_finetuning_traces.py
+++ scripts/extract_finetuning_traces.py
@@ -11,18 +11,18 @@ Notes:
 
 """
 
-import importlib
 import logging
 import os
 import pickle
-import playwright.sync_api as playwright_sync
+import playwright
 
-from browsergym.core.env import BrowserEnv
+from browsergym.core import env
 from browsergym.workarena import ALL_WORKARENA_TASKS
 from collections import defaultdict
 from tenacity import retry, stop_after_attempt, wait_fixed
-from time import sleep
 from time import time
+
+logging.basicConfig(level=logging.INFO)
 
 from browsergym.workarena import (
     DASHBOARD_TASKS,
@@ -46,9 +46,6 @@ ALL_L1_TASKS = (
 )
 
 
-N_PER_TASK = 1
-
-
 def monkey_patch_playwright(observation_callback, trace_storage):
     """
     A function that overrides the default playwright actions to log the actions and observations.
@@ -67,7 +64,7 @@ def monkey_patch_playwright(observation_callback, trace_storage):
         def wrapped(*args, **kwargs):
             # Get the observation
             obs = observation_callback()
-
+            logging.info(args)
             # Get the BID of the element on which we are acting.
             if interface.__name__ == "Locator":
                 # Get the locator
@@ -98,20 +95,30 @@ def monkey_patch_playwright(observation_callback, trace_storage):
 
         return wrapped
 
-    # Interfaces and actions we want to monkey patch
-    importlib.reload(playwright_sync)
-    from playwright.sync_api import Page, Frame, Locator, Keyboard, ElementHandle
-
     # TODO: Make sure the list of interfaces and actions is exhaustive
     #       It covers all that is used in WorkArena cheats as of April 11, 2024
-    interfaces = [Page, Frame, Locator, Keyboard, ElementHandle]
+    interfaces = [
+        playwright.sync_api.Page,
+        playwright.sync_api.Frame,
+        playwright.sync_api.Locator,
+        playwright.sync_api.Keyboard,
+        playwright.sync_api.ElementHandle,
+    ]
     actions = ["click", "select_option", "set_checked", "fill", "press", "type", "down", "up"]
 
+    original_actions = {}
     for interface in interfaces:
         for action in actions:
             if hasattr(interface, action):
+                original_actions[interface, action] = getattr(interface, action)
                 setattr(interface, action, wrapper(getattr(interface, action), interface))
-                print(f"Monkey patched {interface.__name__}.{action}")
+
+    return original_actions
+
+
+def monkey_depatch_playwright(original_actions):
+    for interface, action in original_actions:
+        setattr(interface, action, original_actions[interface, action])
 
 
 # @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
@@ -126,16 +133,18 @@ def extract_trace(task_cls, headless=True):
 
     """
     # Instantiate a new environment
-    env = BrowserEnv(task_entrypoint=task_cls, headless=headless, slow_mo=1000)
+    browser_env = env.BrowserEnv(task_entrypoint=task_cls, headless=headless, slow_mo=1000)
 
     # Setup customized tracing
     trace = []
-    monkey_patch_playwright(observation_callback=env._get_obs, trace_storage=trace)
+    original_actions = monkey_patch_playwright(
+        observation_callback=browser_env._get_obs, trace_storage=trace
+    )
 
-    env.reset()
+    browser_env.reset()
 
     # L1
-    env.task.cheat(env.page, env.chat.messages)
+    browser_env.task.cheat(browser_env.page, browser_env.chat.messages)
 
     # L2
     # for i in range(len(env.task)):
@@ -143,14 +152,16 @@ def extract_trace(task_cls, headless=True):
     #     env.chat.add_message(role="assistant", msg=f'Executing subtask {i}: {env.task.subtasks[i]}')
     #     env.task.cheat(page=env.page, chat_messages=env.chat.messages, subtask_idx=i)
 
-    reward, done, message, info = env.task.validate(page=env.page, chat_messages=env.chat.messages)
-   
-    if reward == 1:
-        env.chat.add_message(role="user", msg="Yes, that works. Thanks!")
-    else:
-        env.chat.add_message(role="user", msg=f"No, that doesn't work. {info.get('message', '')}")
-    env.close()
+    # For gold traces, we don't have to validate the task
+    # reward, done, message, info = env.task.validate(page=env.page, chat_messages=env.chat.messages)
+    # if reward == 1:
+    #     env.chat.add_message(role="user", msg="Yes, that works. Thanks!")
+    # else:
+    #     env.chat.add_message(role="user", msg=f"No, that doesn't work. {info.get('message', '')}")
+    browser_env.close()
 
+    # Restore the original actions. The original code based onimportlib.reload doesn't work here.
+    monkey_depatch_playwright(original_actions)
 
     return trace
 
@@ -158,22 +169,24 @@ def extract_trace(task_cls, headless=True):
 if __name__ == "__main__":
     os.makedirs("trace_profiling", exist_ok=True)
 
+    NUM_TRACES_PER_TASK = 10
+
+    tasks = [t for t in ALL_L1_TASKS if "Order" in str(t)]
+    print("In total, there are", len(tasks), "tasks")
+    for task in tasks:
+        print(task)
+
     task_traces = defaultdict(list)
-    for task in ALL_L1_TASKS:
-        if 'Order' not in str(task):
-            continue
-        print("Task:", task)
-        for i in range(N_PER_TASK):
-            print(f"Extracting trace {i+1}/{N_PER_TASK}")
-            trace = extract_trace(task, headless=False)
-            for index, step in enumerate(trace):
-                print(f"Step {index}")
-                print('Screenshot shape:', step['obs']['screenshot'].shape)
-                element = step['obs']['extra_element_properties'].get(step['bid'])
-                bbox = element['bbox']
-                print('Action:', step['action'], 'BID:', step['bid'], 'Bounding box:', bbox, 'Args:', step['args'])
-                print('')
+    for task in tasks:
+        name = str(task).split(".")[-1][:-2]
+        print("Task:", name)
 
-            task_traces[task].append(trace)
-        break
-
+        with open(f"trace_profiling/{name}.pickle", "wb") as f:
+            for i in range(NUM_TRACES_PER_TASK):
+                print(f"Extracting trace {i+1}/{NUM_TRACES_PER_TASK}")
+                try:
+                    trace = extract_trace(task, headless=True)
+                    pickle.dump(trace, f)
+                except Exception as e:
+                    print(f"Error extracting trace {i+1}/{NUM_TRACES_PER_TASK}: {e}")
+                    input("Press Enter to continue...")
